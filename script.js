1b0628db4f92dd5642a9e91911b68ed9aebcdc87
shahasourav71 <souravshaha53@gmail.com>
2022-04-13 22:42:11 +0600

    new antry

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// deposit part here.......
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    let moneyIn = parseFloat(document.getElementById('deposit').value);
+    let depositIn = parseFloat(document.getElementById('depositIn').innerHTML);
+    let total = moneyIn + depositIn;
+    document.getElementById('depositIn').innerHTML = total;
+    document.getElementById('deposit').value = "";
+})
 
-function pronoun (){
-    
-}
+// withdraw part here...
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    let moneyOut = parseFloat(document.getElementById('withdraw').value);
+    let depositIn = parseFloat(document.getElementById('depositIn').innerHTML);
+    let total = moneyIn + depositIn;
+    document.getElementById('depositIn').innerHTML = total;
+    document.getElementById('deposit').value = "";
+})
+
+
 
 
